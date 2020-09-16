102fe47cad0701516a0990ca9097a5ec17c907df
takedawon <takedaown@naver.com>
2020-09-16 12:13:16 +0900

    Fragment 추가(Activity, Fragment @AndroidEntryPoint Add)

--- app/src/main/java/com/example/study/ui/MainActivity.kt
+++ app/src/main/java/com/example/study/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.study.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
+import com.example.study.BlankFragment
 import com.example.study.R
 import com.example.study.TestViewModel
 import com.example.study.base.BaseActivity
@@ -18,10 +20,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 ) {
 
     private val testViewModel by viewModels<TestViewModel>()
-
+    val blankFragment by lazy { BlankFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = this@MainActivity.testViewModel
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_main, blankFragment)
+            .commitAllowingStateLoss()
     }
+
+
 }
